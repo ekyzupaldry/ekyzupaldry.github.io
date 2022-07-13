85dcd5d588cfa8d4cecdcb9fe1563f1860c678f2
Eky Zupaldry <52279116+ekyzupaldry@users.noreply.github.com>
2022-07-13 10:42:41 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const checkbox = document.querySelector('.hamburger-menu input');
-const ul = document.querySelector('nav ul');
-
-checkbox.addEventListener('click', function (){
-    ul.classList.toggle('open');
-});
